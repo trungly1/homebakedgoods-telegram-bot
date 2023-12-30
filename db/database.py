3287789f0e190cdec583c1943ef42c6d254e8f03
Trung Ly <trung.ly1@pm.me>
2023-12-30 04:48:13 -0500

    wdq

--- db/database.py
+++ db/database.py
@@ -1,10 +1,58 @@
 import sqlite3
-from constants import DB_NAME
+
+# Database file name
+DATABASE_FILE = 'my_telegram_bot.db'
 
 def get_db_connection():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row
+    """Create and return a database connection."""
+    conn = sqlite3.connect(DATABASE_FILE)
     return conn
 
-def close_db_connection(conn):
+def create_tables():
+    """Create tables in the database."""
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            category TEXT NOT NULL,
+            price REAL NOT NULL,
+            quantity INTEGER NOT NULL
+        )
+    ''')
+
+    conn.commit()
     conn.close()
+
+def initialize_data():
+    """Initialize the database with some data."""
+    products = [
+        ('Blueberry Muffin', 'Muffins', 2.50, 30),
+        ('Chocolate Chip Muffin', 'Muffins', 2.75, 25),
+        ('Banana Nut Muffin', 'Muffins', 2.50, 20),
+        ('Chocolate Chip Cookie', 'Cookies', 1.00, 50),
+        ('Oatmeal Raisin Cookie', 'Cookies', 1.00, 40),
+    ]
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.executemany('''
+        INSERT INTO products (name, category, price, quantity)
+        VALUES (?, ?, ?, ?)
+    ''', products)
+
+    conn.commit()
+    conn.close()
+
+def main():
+    """Main function to set up the database."""
+    create_tables()
+    initialize_data()
+    print("Database setup complete with initial data.")
+
+if __name__ == "__main__":
+    main()
+
